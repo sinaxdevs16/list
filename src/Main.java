f4a2f0919987fd84e952e13428f98a2028326b33
sinaxdevs16 <sinadevs16@gmail.com>
2019-12-20 22:41:51 +0330

    list

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        float sum = 0;
+
+        int index = 1;
+
+        List<Float> list = new ArrayList<>();
+
+        float num;
+
+        while (true) {
+
+            System.out.format("nomreye dars [%d] :", index);
+
+            if ((num = scanner.nextFloat()) != -1) {
+                list.add(num);
+                sum += num;
+                index++;
+            } else {
+                break;
+            }
+        }
+
+        float avg = sum / list.size();
+        System.out.format("avg = %.2f \n", avg);
+    }
+}

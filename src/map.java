aa808236075b8f5876260000ab4acfa8cc30a173
sinaxdevs16 <sinadevs16@gmail.com>
2020-02-06 23:52:09 +0330

    solve bug!

--- src/map.java
+++ src/map.java
@@ -30,31 +30,6 @@ public class map {
                 break;
             }
         }
-//        for (int i = 0; i < 3; i++) {
-//            //get key :
-//            System.out.format("enter lesson name [%d} : ", index);
-//            String name = scanner.next().toString();
-//
-//            //get value  :
-//            System.out.format("enter grade of lesson [%d} : ", index);
-//            float grade = scanner.nextFloat();
-//
-//            hashMap.put(name, grade);
-//
-//            index++;
-//        }
-
-//        hashMap.forEach((key, value) ->
-//        {
-//            sum += hashMap.get(key);
-//            System.out.println();
-//            System.out.format(" the grade of lesson %s is --> %.2f", key, value);
-//        });
-//        System.out.println();
-//        System.out.format("the sum of grades is %.2f", sum);
-//        for (int i = 0; i < hashMap.size(); i++) {
-//            sum += hashMap.values();
-//        }
         if (hashMap.size() != 0) {
             float avg = sum / hashMap.size();
             System.out.format("avg = %.2f \n", avg);
